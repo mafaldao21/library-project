c37deb668a7f45cd9448b41cd7c8d0e261242551
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-04-25 16:12:55 +0200

    CREATE: functionality to create new books

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -14,6 +14,31 @@ router.get("/books", (req, res, next) => {
 });
 
 
+router.get("/books/create", (req, res, next) => {
+    res.render("books/book-create");
+})
+
+router.post("/books/create", (req, res, next) => {
+    
+    const newBook = {
+        title: req.body.title,
+        description: req.body.description,
+        author: req.body.author,
+        rating: req.body.rating,
+    }
+
+    Book.create(newBook)
+        .then((bookFromDB) => {
+            res.redirect("/books");
+        })
+        .catch(err => {
+            console.log("error creating book on DB", err)
+            next(err);
+        });
+
+})
+
+
 router.get("/books/:bookId", (req, res, next) => {
     const id = req.params.bookId;
 
@@ -26,5 +51,7 @@ router.get("/books/:bookId", (req, res, next) => {
             next(err);
         });
 })
+
+
 
 module.exports = router;
